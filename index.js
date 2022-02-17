fa819f1457854ac4fc791f3cb96f467d03b53cfc
UglyValfi <ValfiBrind@gmail.com>
2022-02-17 15:44:07 +0200

    f

---
+++ index.js
@@ -0,0 +1,51 @@
+// for (let i = 0; i <= 100; i++) {   
+//     console.log(i)
+// }
+
+// for (let i = 11; i <= 33; i++) {   
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i <= 4; i++) {
+// 	console.log(arr[i]);
+// }
+
+// let arr = [1,2,3,4,5];
+// function arraySum(array){
+// let sum = 0;
+// for(let i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// console.log(sum);
+// }
+// arraySum(arr);
+
+// let obj = {green: 'зеленый', red: 'красный', blue: 'голубой'}
+// console.log(obj)
+
+
+// let styles = ["Джаз", "Блюз"]
+// styles.push("Рок-н-рол")
+// styles[1]="Классика"
+// styles.shift()
+// styles.unshift("Реп","Реггі")
+// console.log(styles)
+
+// var pr=[]; // объявить массив
+// for(var i = 0; i<3; i++) pr.push(prompt());// записать значения в массив
+// for(var i = 0; i<3; i++) document.write(pr[i]+" ");// вывести значения массива на экран
